98a747d71287751683c23b8ba5f5da3e89a63b95
Mac <glee4810@gmail.com>
2024-01-22 17:27:14 +0900

    update preprocessing

--- preprocess/preprocess.sh
+++ preprocess/preprocess.sh
@@ -1,9 +1,10 @@
 python3 preprocess_db.py \
---data_dir /nfs_data_storage/mimic-iv-2.2-demo \
+--data_dir ../mimic-iv-clinical-database-demo-2.2 \
 --db_name mimic_iv \
 --num_patient 100 \
 --timeshift \
 --current_time "2100-12-31 23:59:00" \
 --start_year 2100 \
 --time_span 0 \
+--out_dir ../data \
 --cur_patient_ratio 0.1
